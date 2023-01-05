c08c544facf84fceb7729a1b30cd94eb6267f08d
Khajavali20 <sayyed.khajavali1234@gmail.com>
2023-01-05 17:44:22 +0000

    Shell scipt first example

---
+++ ScriptShell/first.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+echo "Hello Guys!!"
+echo "Hello   world"
+echo  Hello           World
+echo "Hello "*" World"
+name="narendra"
+age="30"
+echo "job is"
+
+echo "My name is:"$name
+echo  My AGE is:$age
+echo "job type is :"$job
+echo TERM
+echo "MY NAME IS: "$
+echo "MY AGE IS: "$
